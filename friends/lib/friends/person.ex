f45da74e0818df2ad3045b9a2340ba785b040cd7
Vagrant Default User <vagrant@buster.localdomain>
2020-12-02 04:49:13 +0000

    first commit. phoenix-exlixir-postgresql setup.

---
+++ friends/lib/friends/person.ex
@@ -0,0 +1,23 @@
+defmodule Friends.Person do
+  use Ecto.Schema
+
+  schema "people" do
+    field :first_name, :string
+    field :last_name, :string
+    field :age, :integer
+  end
+
+  def changeset(person, params \\ %{}) do
+    person
+    |> Ecto.Changeset.cast(params, [:first_name, :last_name, :age])
+    |> Ecto.Changeset.validate_required([:first_name, :last_name])
+  end
+
+    # case Friends.Repo.insert(changeset) do
+    #     {:ok, person} ->
+    #     # do something with person
+    #     {:error, changeset} ->
+    #     # do something with changeset
+    # end
+end
+
